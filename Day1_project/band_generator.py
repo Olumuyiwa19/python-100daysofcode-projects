eb028a9a16678135a9059652ca5237da629df88f
Olumuyiwa19 <kolayemi.olumuyiwa@gmail.com>
2023-09-26 12:32:21 -0400

    develop band generator app

---
+++ Day1_project/band_generator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Welcome to Band Generator app")
+
+#2. Ask the user for the city that they grew up in.
+city_name = input("Kindly provide the city you grew up here: \n")
+#print(city_name)
+#3. Ask the user for the name of a pet.
+pet_name = input("Kindly provide the name of your pet here: \n")
+#print(pet_name)
+#4. Combine the name of their city and pet and show them their band name.
+band_name = city_name +" "+ pet_name
+#5. Make sure the input cursor shows on a new line:
+print("Your band name could be" +" "+ band_name)
