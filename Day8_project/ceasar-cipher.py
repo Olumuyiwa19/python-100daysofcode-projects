48be57662733b40643edde89de9fb02d52337276
Olumuyiwa19 <kolayemi.olumuyiwa@gmail.com>
2023-11-13 16:07:36 -0500

    update with comments for easy readability

--- Day8_project/ceasar-cipher.py
+++ Day8_project/ceasar-cipher.py
@@ -47,13 +47,16 @@ def caesar_cipher(text, shift, direction):
 
 replay = True
 while replay:
+    #get input from user about their choice encode or decode
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
+    #encrypt/decrypt the text
     result = caesar_cipher(text, shift, direction)
     print(f"The {direction}d text is: {result}")
 
+    #allow user to continue or exit the program
     choice = input("Do you want to go again? Type 'yes' or 'no': ")
     if choice.lower() != 'yes':
-        replay = False
+        replay = False    #exit the loop if the user doesn't want to go again
